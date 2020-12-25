dd654327ab96e32529177b0457d084ab203f3a41
Puja Sharma <55825774+Puja-Sh@users.noreply.github.com>
2020-12-25 15:08:36 +0530

    Add files via upload

---
+++ src/components/shared/InfiniteLoading.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import InfiniteLoading from 'react-simple-infinite-loading';
+
+class InfiniteScroll extends React.Component {
+    render() {
+        return <div />;
+    }
+}
+
+export default InfiniteLoading;
+
